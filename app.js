51544fd5544677f0915e9bf6dab17f5209921fb7
William <24wferchawsmiley@leisdstudent.ws>
2022-03-31 14:54:06 -0500

    Even More Reasonable

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
 function askName() {
     var uname = prompt("What is your name?")
-    alert(`Hello ${uname} and Welcome to 20! This is a competition to see which player gets closer to the number 20. You are going to be playing against the computer. \nYou will draw numbers from 1 to 10. The computer will stop at 16. Pay attention to the screen to see what your total is, and if you need another number press 'y'`);
+    alert(`Hello ${uname} and Welcome to 20! This is a competition to see which player gets closer to the number 20. You are going to be playing against the computer. \n\nYou will draw numbers from 1 to 10. The computer will stop at 16. Pay attention to the screen to see what your total is, and if you need another number press 'y'`);
 }
 
 function RNG() 
 {
         var RNG = Math.floor(Math.random() * 10);
-        var nTotal = nTotal + RNG;
+        return RNG
 }
 
 function userG() 
-{
-    do 
-    {
-        RNG()
-        var uTotal = nTotal
-    }while (uTotal < 16)
+{   
+    do {
+        play = RNG()
+        ptotal = ptotal + play;
+
+        alert(`Your new number is '${play}'. \nThe Players total is now '${ptotal}'`);
+            
+        if (ptotal > 20) {
+            alert("Player Lost");
+            cwin++
+            break;
+        }
+            
+        ans = prompt("Do you want another number? 'y' for yes, 'n' for no.")
+        
+    }while (ans == "y");
+    return ptotal
 }
 
 function computerG() 
 {
-    do 
-    {
-        RNG()
-        var cTotal = nTotal
-    }while (cTotal < 16)
+    do {
+        comp = RNG()
+        ctotal = ctotal + comp;
+    
+        alert(`The Computer's new number is '${comp}. \nThe Computer's total is now '${ctotal}''`)
+        
+    }while (ctotal < 16);
+    return ctotal
 }
 
-function compareCG() {
-    if ((cTotal > uTotal) && (cTotal <= 20)){
-    alert(`${uname} Lost... You got ${uTotal} and the Computer got ${cTotal}`);
-    cWin++
-    }else if ((uTotal > cTotal) && (uTotal <= 20)) {
-    alert(`${uname} win! You got ${uTotal} and the computer got ${cTotal}`);
-    uWin++
+function compareCU(uname, cTotal, uTotal) {
+    if ((cTotal > uTotal) && (cTotal <= 20))
+    {
+        alert(`${uname} Lost... You got ${uTotal} and the Computer got ${cTotal}`);
+        var cWin = 0;
+        cWin++
+    }else if ((uTotal > cTotal) && (uTotal <= 20)) 
+    {
+        alert(`${uname} win! You got ${uTotal} and the computer got ${cTotal}`);
+        var uWin = 0;
+        uWin++
     }
 
     
@@ -45,5 +63,12 @@ function compareCG() {
 
 function game() {
 
-    compareCG(computerG(), userG(askName()))
+    compareCU(askName(), userG(), computerG())
 }
+
+game()
+
+
+
+
+
