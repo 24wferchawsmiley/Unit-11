6aa98fca1a85451ecb94afd16454c28cedac5a19
William <24wferchawsmiley@leisdstudent.ws>
2022-04-01 14:54:51 -0500

    So reasonable it works

--- app.js
+++ app.js
@@ -1,74 +1,86 @@
+var cWin = 0;
+var uWin = 0;
+var ans = "";
+var uTotal = 0;
 function askName() {
     var uname = prompt("What is your name?")
     alert(`Hello ${uname} and Welcome to 20! This is a competition to see which player gets closer to the number 20. You are going to be playing against the computer. \n\nYou will draw numbers from 1 to 10. The computer will stop at 16. Pay attention to the screen to see what your total is, and if you need another number press 'y'`);
+    return uname
 }
 
-function RNG() 
-{
-        var RNG = Math.floor(Math.random() * 10);
-        return RNG
-}
 
 function userG() 
 {   
+    var utotal = 0;
     do {
-        play = RNG()
-        ptotal = ptotal + play;
+        var user = Math.floor(Math.random() * 10);
+        
+        utotal = utotal + user;
 
-        alert(`Your new number is '${play}'. \nThe Players total is now '${ptotal}'`);
+        alert(`Your new number is '${user}'. \nThe Players total is now '${utotal}'`);
             
-        if (ptotal > 20) {
+        if (utotal > 20) {
             alert("Player Lost");
-            cwin++
+            cWin++
             break;
         }
             
-        ans = prompt("Do you want another number? 'y' for yes, 'n' for no.")
+        ques = prompt("Do you want another number? 'y' for yes, 'n' for no.")
         
-    }while (ans == "y");
-    return ptotal
+    }while (ques == "y");
+    return utotal
 }
 
-function computerG() 
+function computerG(a) 
 {
+    var ctotal = 0;
     do {
-        comp = RNG()
+        if (a > 20) {
+            cWin++
+            break;
+        }
+        var comp = Math.floor(Math.random() * 10);
         ctotal = ctotal + comp;
     
-        alert(`The Computer's new number is '${comp}. \nThe Computer's total is now '${ctotal}''`)
+        alert(`The Computer's new number is '${comp}. \nThe Computer's total is now '${ctotal}'`)
+
+        if (ctotal > 20) {
+            alert("Computer Lost");
+            uWin++
+            break;
+        }
         
     }while (ctotal < 16);
     return ctotal
 }
 
-function compareCU(uname, cTotal, uTotal) {
+function compareCU(a, cTotal) 
+{
     if ((cTotal > uTotal) && (cTotal <= 20))
     {
-        alert(`${uname} Lost... You got ${uTotal} and the Computer got ${cTotal}`);
-        var cWin = 0;
+        alert(`${a} Lost... You got ${uTotal} and the Computer got ${cTotal}`);
         cWin++
     }else if ((uTotal > cTotal) && (uTotal <= 20)) 
     {
-        alert(`${uname} win! You got ${uTotal} and the computer got ${cTotal}`);
-        var uWin = 0;
+        alert(`${a} won! You got ${uTotal} and the computer got ${cTotal}`);
         uWin++
     }
 
     
-    return cWin, uWin
+    ans = prompt(`The current score is '${a}' '${uWin}' : 'Computer' '${cWin}' Do you want to play again? \n\n ('y' for yes) ('n' for no)`)
+    return ans
 }
 
 
 
 
-function game() {
-
-    compareCU(askName(), userG(), computerG())
+function gm() 
+{
+    do {
+    compareCU(askName(), computerG(userG()), )
+    }while (ans == "y")
 }
 
-game()
 
 
-
-
-
+gm()
